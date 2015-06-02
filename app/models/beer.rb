8805469f3f80b455bfba70a43f889f3be8d9021c
kwokster10 <kwokster10@hotmail.com>
2015-06-02 12:11:55 -0400

    updated models

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,5 @@
 class Beer < ActiveRecord::Base
+  belongs_to :user, dependent: :destroy
   validates :name, presence: true
   validates_length_of :description, :minimum => 10, :maximum => 50
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :beers
   validates :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: {message: "I know you"}
 end
